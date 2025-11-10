753dbb2e9045be05c4999f8ddc6470acd0494708
Spoorthi R Hegde <spoorthia084@gmail.com>
2025-11-10 15:21:13 +0530

    Added successfully

--- backend/app.py
+++ backend/app.py
@@ -13,10 +13,10 @@ from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 
 # Configure Gemini API
-genai.configure(api_key="AIzaSyC35sQNpirJ-i0TumT3mk2EFoHdCZLpSU0")
+genai.configure(api_key="AIzaSyCKIvRMGbvVZaN1gbbNGXYcSlv0Cl_dSRc")
 
 chat_model = genai.GenerativeModel(
-    model_name="gemini-1.5-flash",
+    model_name="gemini-2.5-flash",
     generation_config={"temperature": 0.7, "top_p": 1, "top_k": 1},
     safety_settings=[
         {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
